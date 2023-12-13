baca9f81affe740053a73dd214034f0b1abfb290
ng-aman <aman.roland@ngenux.com>
2023-12-13 11:22:54 +0530

    added programs for add & sub

---
+++ program/addition.py
@@ -0,0 +1,3 @@
+def adding_numbers(num_1, num_2):
+    summation = num_1 + num_2
+    return summation
---
+++ program/subtraction.py
@@ -0,0 +1,3 @@
+def subtract_numbers(num_1, num_2):
+    subtraction = num_1 - num_2
+    return subtraction
